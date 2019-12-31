f8c40e47dae5157f7bd2f4b50bd870d847aae4bb
MCLonips <franken-tim@mail.de>
2019-12-31 17:24:31 +0100

    Fixed untracked files

--- capella-service/src/main/java/de/mclonips/clubadministration/capella/controller/ICapellaController.java
+++ capella-service/src/main/java/de/mclonips/clubadministration/capella/controller/ICapellaController.java
@@ -1,7 +1,5 @@
 package de.mclonips.clubadministration.capella.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +12,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 /**
  * @author Tim Franken (FRTI)
  */
-@Api(value = "CapellaService", description = "Service to convert a valid Capella .capx-file to an excel file")
 public interface ICapellaController {
 
     @PostMapping(
@@ -22,7 +19,6 @@ public interface ICapellaController {
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
     )
-    @ApiOperation(value = "Converts a given and valid .capx-file to an excel file")
     ResponseEntity<ByteArrayResource> convertFile(@RequestParam("file") final MultipartFile file, final RedirectAttributes redirectAttributes);
 
     @GetMapping(path = "/version", produces = MediaType.TEXT_PLAIN_VALUE)
